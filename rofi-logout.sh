02d1d715eb80f2d33fff84ce4b23b86cf21c3ff5
Captain Asbestos <71844282+CaptainAsbestos@users.noreply.github.com>
2021-07-23 22:46:13 -0500

    Create rofi-logout.sh

---
+++ rofi-logout.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+question=$(echo "lock screen|kill Xserver|logout|reboot|shutdown" | rofi -sep "|" \
+    -dmenu -i -p 'System' "" -width 27 -hide-scrollbar \
+    -eh 1 -line-padding 4 -font "Hack 16" -padding 20 -lines 5 -yoffset 100 -color-enabled)
+
+case $question in
+    *"lock screen")
+        solarlock.sh
+        ;;
+    *"kill Xserver")
+        i3-msg exit
+        ;;
+    *logout)
+        kill -9 -1
+        ;;
+    *reboot)
+        systemctl reboot
+        ;;
+    *shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        exit 0  # do nothing on wrong response
+        ;;
+esac
